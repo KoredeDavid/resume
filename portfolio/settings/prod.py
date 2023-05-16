751d39330ba2a1b599249ad3c0d82a5416db42af
KoredeDavid <koredeoluwashola@gmail.com>
2023-05-16 08:54:35 +0100

    Update allowed host

--- portfolio/settings/prod.py
+++ portfolio/settings/prod.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False) == 'True'
 
-ALLOWED_HOSTS = ['korededavid.herokuapp.com']
+ALLOWED_HOSTS = ['korededavid.herokuapp.com', 'korededavid.com', 'www.korededavid.com']
 
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware', )
 
